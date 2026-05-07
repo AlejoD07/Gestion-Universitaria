5f710504f2274fd12bccbd66a015021bbf55186e
Santiago.L <santiagoleon009@gmail.com>
2026-05-07 16:58:52 -0500

    Se agrego el app_name a la aplicacion RecursosHumanos

--- RecursosHumanos/urls.py
+++ RecursosHumanos/urls.py
@@ -1,6 +1,8 @@
 ﻿from django.urls import path
 from . import views
 
+app_name = "rrhh"
+
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('inicio/', views.inicio, name='inicio'),
